9a9610036756f0c45737dc56b302e9e9589185d1
vanessachuaaa <61442233+vanessachuaaa@users.noreply.github.com>
2020-03-03 10:14:34 +0800

    GroceryProject
    Week6 Grocery Project with data passed from App.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <app-header></app-header>
+    <PageContent></PageContent>
+    <app-footer></app-footer>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//Registering components Locally
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import PageContent from './components/PageContent.vue'
 export default {
   name: 'App',
+  data(){
+    return{
+        itemsList: [{name:'Singapore Flyer',image:'./assets/SingaporeFlyer.jfif',show:false},
+                    {name:'Art Science Museum',image:'/assets/ArtScienceMuseum.jfif',show:false},
+                    {name:'Merlion',image:'/assets/Merlion.jfif',show:false},
+                    {name:'Singapore River',image:'/assets/SingaporeRiver.jfif',show:false},
+                    {name:'Gareden By The Bay',image:'/assets/GaredenByTheBay.jfif',show:false},
+                    {name:'Jewel Changi',image:'/assets/JewelChangi.jfif',show:false}]
+        }
+  },
   components: {
-    HelloWorld
+    //Registering with a name
+    'app-header':Header,
+    'app-footer':Footer,
+    //Registering without a name
+    PageContent
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="headerStyle">
+    <footer>
+        <p v-html="copyRtSymbol"></p>
+                
+    </footer>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data(){
+    return{
+        
+        copyRtSymbol:'&copy;Boxin_BT3103'
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer{
+    background:rgb(13, 87, 167);
+    padding:10px;
+    border-style: solid;
+    border-color:black;
+    border-width: 2px;
+}
+p{
+    align-content: center;
+    color:yellow;
+}
+</style>
